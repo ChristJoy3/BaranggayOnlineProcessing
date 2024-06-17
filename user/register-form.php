fab50d1a85fc26ed4caefd18f500849d287cba54
cj <christjoy2022@gmail.com>
2024-06-17 10:40:26 +0800

    9

--- user/register-form.php
+++ user/register-form.php
@@ -35,10 +35,7 @@ if (isLoggedIn()) {
                     <label for="exampleInputPassword1" class="form-label">Password</label>
                     <input type="password" name="password" class="form-control" id="exampleInputPassword1" required>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputFile" class="form-label">Profile Picture</label>
-                    <input type="file" name="profile_picture" class="form-control" id="exampleInputFile" accept="image/*">
-                </div>
+                
                 <div class="d-grid gap-2 col-12 mt-3">
                     <input class="btn btn-primary" type="submit" value="Register">
                     <a href="index.php" class="btn btn-secondary">Back</a>
